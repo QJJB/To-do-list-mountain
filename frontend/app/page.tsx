b4e1bbc4aed80d102194f4bcec71e84c49df1ad5
Gondain <quueenntiin@gmail.com>
2025-03-13 13:38:52 +0100

    first commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,15 @@
+import {get} from "@/server/user"
+
+export default async function Home() {
+  const data = await get();
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <div>
+        {data.map((user) => (
+          <p key={user.id}>{user.email}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ frontend/server/user.ts
@@ -0,0 +1,10 @@
+interface User {
+    email:string;
+    id:string;
+}
+
+export const get = async (): Promise<User[]> => {
+    const data = await fetch(`${process.env.APP_URL}/users`)
+    const json = await data.json()
+    return json.data
+}
