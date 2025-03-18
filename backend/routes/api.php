1af259d51624a566285f69b503ee67fa3947b871
Gondain <quueenntiin@gmail.com>
2025-03-18 15:19:57 +0100

    connexion avec la base de données en ligne

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,9 @@
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
 Route::get('/users', function () {
     return UserResource::collection(User::all());
 });
+
