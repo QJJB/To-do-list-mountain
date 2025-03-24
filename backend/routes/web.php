fcd4ff20805ed0658981246990e965ad486dc751
Bastien Morrier <bastien.morrier@gmail.com>
2025-03-24 15:39:52 +0100

    ajout des fonctionnalités de bastien

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use App\Models\Project;
 use Inertia\Inertia;
+use \App\Http\Controllers\ProjectController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -23,5 +25,28 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/test', [ProjectController::class, 'project'])->middleware('auth');
+
+Route::post('/test', [ProjectController::class, 'projectPost'])->middleware('auth');
+
+Route::get('/home', function(){
+    return view('home', [
+        'projects' => Project::all()
+    ]);
+})->middleware('auth');
+
+Route::get('/home/{id}', function($id){
+    $project = Arr::first(Project::all(), fn($job) => $job['id'] ==$id);
+
+    if(! $project){
+        abort(404);
+    }
+
+    return view('project', [
+        'projects' => $project
+    ]);
+})->middleware('auth');
+
 
 require __DIR__.'/auth.php';
