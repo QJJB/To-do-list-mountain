e785345f29c83ef02a18b85cda98250b0ecb900d
Bastien Morrier <bastien.morrier@gmail.com>
2025-03-24 15:53:16 +0100

    bastien commit merge

--- backend/app/Http/Controllers/ProfileController.php
+++ backend/app/Http/Controllers/ProfileController.php
@@ -3,38 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
-<<<<<<< HEAD
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-=======
->>>>>>> 6089fa36348632ee7f80c95b3ff2d9bcc5b60332
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-<<<<<<< HEAD
 use Inertia\Inertia;
 use Inertia\Response;
-=======
-use Illuminate\View\View;
->>>>>>> 6089fa36348632ee7f80c95b3ff2d9bcc5b60332
 
 class ProfileController extends Controller
 {
     /**
      * Display the user's profile form.
      */
-<<<<<<< HEAD
     public function edit(Request $request): Response
     {
         return Inertia::render('Profile/Edit', [
             'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
             'status' => session('status'),
-=======
-    public function edit(Request $request): View
-    {
-        return view('profile.edit', [
-            'user' => $request->user(),
->>>>>>> 6089fa36348632ee7f80c95b3ff2d9bcc5b60332
         ]);
     }
 
@@ -51,11 +37,7 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-<<<<<<< HEAD
         return Redirect::route('profile.edit');
-=======
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
->>>>>>> 6089fa36348632ee7f80c95b3ff2d9bcc5b60332
     }
 
     /**
@@ -63,11 +45,7 @@ class ProfileController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
-<<<<<<< HEAD
         $request->validate([
-=======
-        $request->validateWithBag('userDeletion', [
->>>>>>> 6089fa36348632ee7f80c95b3ff2d9bcc5b60332
             'password' => ['required', 'current_password'],
         ]);
 
